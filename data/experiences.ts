4377806f3b0d11573a089bfa205733b1426c9659
Hayden ONeill <haydenoneill5@icloud.com>
2025-05-23 23:26:40 +0100

    Feat: Update experience section with new job details and add logo for Canny Creative

--- data/experiences.ts
+++ data/experiences.ts
@@ -8,17 +8,17 @@ export interface Experience {
 
 export const experiences: Experience[] = [
   {
-    title: "Team Member",
-    company: "Taco Bell",
-    period: "2024 - Present",
+    title: "Junior Front End Web Developer",
+    company: "Canny Creatice",
+    period: "June 2025 - Present",
     description:
-      "Work in a fast-paced environment, adapting to both customer service and food preparation tasks. Collaborate with team members to ensure smooth operations and maintain high standards of service and efficiency.",
-    logoPath: "images/experience/tacobell.png",
+      "Working as part of a team to service client accounts and create websites, working closely with the design and content teams to bring their ideas to life.",
+    logoPath: "images/experience/cannycreative.jpeg",
   },
   {
     title: "Table Official",
     company: "British Basketball League / Super League Basketball",
-    period: "2022 - Present",
+    period: "April 2022 - Present",
     description:
       "Oversee professional basketball games as a Level 3 (soon to be Level 4) table official, including high-profile events like the Women's Trophy Final. Develop strong communication skills while working closely with coaches, referees, and league officials.",
     logoPath: "images/experience/bblslb.png",
@@ -26,7 +26,7 @@ export const experiences: Experience[] = [
   {
     title: "Referee / Table Official",
     company: "Basketball England",
-    period: "2021 - Present",
+    period: "August 2021 - Present",
     description:
       "Officiate games from the Junior National Basketball League up to Division 1 National Basketball League as a Level 3 table official and Level 2 referee. Manage high-pressure situations while ensuring fair play and effective teamwork.",
     logoPath: "images/experience/basketballengland.png",
@@ -34,15 +34,23 @@ export const experiences: Experience[] = [
   {
     title: "Referee / Table Official",
     company: "Newcastle Eagles",
-    period: "2019 - Present",
+    period: "November 2019 - Present",
     description:
       "Officiate and mentor officials in the local central venue league as a Level 2 referee. Host CPD sessions to support the development of new referees and table officials, helping them gain qualifications and improve their skills.",
     logoPath: "images/experience/newcastleeagles.png",
   },
   {
+    title: "Team Member",
+    company: "Taco Bell",
+    period: "June 2024 - June 2025",
+    description:
+      "Work in a fast-paced environment, adapting to both customer service and food preparation tasks. Collaborate with team members to ensure smooth operations and maintain high standards of service and efficiency.",
+    logoPath: "images/experience/tacobell.png",
+  },
+  {
     title: "Temporary Sales Assistant",
     company: "Clarks",
-    period: "2023",
+    period: "July 2023 - September 2023",
     description:
       "Provided excellent customer service by assisting with shoe fittings and product recommendations. Managed stockroom operations efficiently and handled customer orders, receiving positive feedback from both customers and managers.",
     logoPath: "images/experience/clarks.png",
@@ -50,7 +58,7 @@ export const experiences: Experience[] = [
   {
     title: "Volunteer Coach",
     company: "Newcastle Eagles",
-    period: "2018 - 2019",
+    period: "September 2018 - September 2019",
     description:
       "Led weekly training sessions for a local basketball team, coaching players and managing teams in competitive games. Communicated progress with parents and contributed to player development.",
     logoPath: "images/experience/newcastleeagles.png",
